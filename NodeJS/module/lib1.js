283878c91f6571870c6db642d22f968e8388474b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-02 17:12:39 +0530

    both ways

--- NodeJS/module/lib1.js
+++ NodeJS/module/lib1.js
@@ -3,7 +3,10 @@ let x=10;
 function add(a, b){
     return a + b;
 }
-const _x = x;
-export { _x as x }; //module.exports is an object that is passed to the file where it is required.
-const _add = add;
-export { _add as add };
+// const _x = x; //new method
+// export { _x as x }; //module.exports is an object that is passed to the file where it is required.
+// const _add = add;
+// export { _add as add };
+
+module.exports.x=x; //previous method
+module.exports.add.add;
