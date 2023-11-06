71366cd1c6a0b6276f2c43daef49f26812bd6c6e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-06 16:54:09 +0530

    Read File Function of File System Module

--- NodeJS/fileHandfling/script.js
+++ NodeJS/fileHandfling/script.js
@@ -2,9 +2,9 @@
 let fs = require("fs");
 // console.log(__dirname);
 let path = require("path");
-let fileName = "data2.txt";
-let filePath = path.join(__dirname, "data", fileName);
-console.log(filePath);
+let fileName = "data.txt";
+let filePath = path.join(__dirname, "Data", fileName);
+// console.log(filePath);
 
 // fs.writeFile(
 //     "./fileHandfling/Data/data.txt",
@@ -20,16 +20,39 @@ console.log(filePath);
 //     }
 // )
 
-fs.writeFile(
+// fs.writeFile(
+//     filePath,
+//     "Hello World Again!",
+//     {
+//         encoding: "utf-8",
+//         flag: "w"
+//     },
+//     (err) => {
+//         if (err) {
+//             console.log(err.message);
+//         }
+//     }
+// )
+
+// fs.rm(
+//     filePath,
+//     {
+//         force: false
+//     },
+//     (err)=>{
+//         if(err){
+//             console.log(err.msg);
+//         }
+//     }
+// )
+
+fs.readFile(
     filePath,
-    "Hello World Again!",
-    {
-        encoding: "utf-8",
-        flag: "w"
-    },
-    (err) => {
-        if (err) {
-            console.log(err.message);
+    (err,data)=>{
+        if(err){
+            console.log("This is an Error");
+        }else{
+            console.log(data.toString());
         }
     }
 )
