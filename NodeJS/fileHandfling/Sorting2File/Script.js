e1dce05e0941a67aae4bc898a208b8d57b897a87
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-06 17:56:00 +0530

    Updated and Formatted Code

--- NodeJS/fileHandfling/Sorting2File/Script.js
+++ NodeJS/fileHandfling/Sorting2File/Script.js
@@ -1,24 +1,24 @@
-let fs=require("fs");
-let path=require("path");
+let fs = require("fs");
+let path = require("path");
 
-let fileName1="File1.txt";
-let fileName2="File2.txt";
+let fileName1 = "File1.txt";
+let fileName2 = "File2.txt";
 
-let filePath1=path.join(__dirname, fileName1);
-let filePath2=path.join(__dirname, fileName2);
+let filePath1 = path.join(__dirname, fileName1);
+let filePath2 = path.join(__dirname, fileName2);
 
-function read(file){
-    return new Promise((resolve, reject)=>{
+function read(file) {
+    return new Promise((resolve, reject) => {
         fs.readFile(
             file,
             {
                 encoding: "utf-8"
             },
-            (err, data)=>{
-                if(err){
+            (err, data) => {
+                if (err) {
                     return reject(err.message);
                 }
-                else{
+                else {
                     resolve(data);
                 }
             }
@@ -26,21 +26,21 @@ function read(file){
     })
 }
 
-read(filePath1).then((data1)=>{
+read(filePath1).then((data1) => {
     // console.log(data1);
-    data1=data1.split("\r\n");
-    read(filePath2).then((data2)=>{
-        data2=data2.split("\r\n");
-        let res=[...data1, ...data2];
+    data1 = data1.split("\r\n");
+    read(filePath2).then((data2) => {
+        data2 = data2.split("\r\n");
+        let res = [...data1, ...data2];
         // console.log(res);
-        res=res.map((element)=>(
+        res = res.map((element) => (
             parseInt(element)
         ))
         console.log(res);
-    //     // console.log(data2);
-    //     let res=data1+data2;
-    //     // console.log(res);
-    //     res=res.parseInt(split("\r\n"));
-    //     console.log(res);
+        //     // console.log(data2);
+        //     let res=data1+data2;
+        //     // console.log(res);
+        //     res=res.parseInt(split("\r\n"));
+        //     console.log(res);
     })
 })
