e11a99caf08f83dde757895810e2f449782b95ca
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-23 17:41:20 +0530

    Displaying Dynamic Data

--- Server Side Rendering/script.js
+++ Server Side Rendering/script.js
@@ -6,7 +6,14 @@ app.set('view engine', 'hbs');
 
 app.get("/home", (req, res)=>{
     res.render("home");
-})
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about", {
+        name: "Sparsh Yadav",
+        age: 21  
+    });
+});
 
 app.listen(3333, ()=>{
     console.log("Server Started");
