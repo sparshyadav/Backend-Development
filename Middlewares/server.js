6b798041864551c6ec64f9c5057fb65820d9adf5
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-04 17:42:21 +0530

    Creating a Middleware using app.use

--- Middlewares/server.js
+++ Middlewares/server.js
@@ -1,13 +1,20 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-function m1(){
+function m1(res, res, next) {
     console.log("Running Middleware 1");
+    next();
 }
+app.use(m1);
 
-app.get("/", (req, res)=>{
+app.get("/about", (req, res) => {
+    console.log("Running /about");
+    res.send("About");
+})
+
+app.get("/", (req, res) => {
     console.log("Running /");
     res.send("home");
 })
 
-app.listen(3333, ()=>console.log("Server Started"));
+app.listen(3333, () => console.log("Server Started"));
