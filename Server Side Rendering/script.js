a63941c0e46ddde2357d731a94847dd6a5b86801
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-04 17:03:03 +0530

    Loop in HBS

--- Server Side Rendering/script.js
+++ Server Side Rendering/script.js
@@ -4,6 +4,13 @@ let app=express();
 
 app.set('view engine', 'hbs');
 
+let todos=[
+    {name:"Cricket", id:1},
+    {name:"Rugby", id:2},
+    {name:"Football", id:3},
+    {name:"Hockey", id:4}
+]
+
 app.get("/home", (req, res)=>{
     res.render("home");
 });
@@ -14,6 +21,12 @@ app.get("/about", (req, res)=>{
         age: 21  
     });
 });
+
+app.get("/todos", (req, res)=>{
+    res.render("todos", {
+        todos:todos
+    });
+})
 
 app.listen(3333, ()=>{
     console.log("Server Started");
