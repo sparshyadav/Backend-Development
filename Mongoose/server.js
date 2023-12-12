63b207cd88f69902c3f242fab5bc08bf4806e8c2
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-12 17:31:58 +0530

    Creating a Schema

--- Mongoose/server.js
+++ Mongoose/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-
-app.use(express.static(path.join(__dirname, "static")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express, json());
+let Kitten = require("./model/kitty");
 
 let dbName = "myDB";
 
 let app = express();
+
+// app.use(express.static(path.join(__dirname, "static")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/api/addkitty", async (req, res) => {
+    const { name } = req.body;
+    let newKitty = new Kitten({ name });
+    await newKitty.save();
+})
 
 mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`)
     .then(() => {
