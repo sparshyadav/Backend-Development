03110ba6961a4e36e4e3208a3785c28b32b425f7
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-15 17:00:05 +0530

    Backend Folder Made

---
+++ Mongoose2/script.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+
+
+app.listen(3334, () => {
+    console.log("Server Started");
+})
